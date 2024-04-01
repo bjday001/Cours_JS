cee8b8cad8eeb6040d162b722d815c6985a56f42
bjday001 <jdayinvestis@gmail.com>
2024-04-01 17:48:39 +0200

    test_queryselector_span

--- script.js
+++ script.js
@@ -8,3 +8,6 @@ let listeInputRadio = document.querySelectorAll(".zoneChoix input");
 for (let i = 0; i < listeInputRadio.length; i++) {
     console.log(listeInputRadio[i]);
 }
+
+let baliseZonePropositionSpan = document.querySelector("#zoneProposition span");
+console.log(baliseZonePropositionSpan);
