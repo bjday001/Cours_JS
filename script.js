17fffc831dbf657e3c7cd1a10f7a1d99f54bf6e5
bjday001 <jdayinvestis@gmail.com>
2024-04-01 17:15:53 +0200

    test getElementById

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
-// Exercice pour manipuler les boucles for 
-const listeMots = ["Cachalot", "Pétunia" ,"Serviette"]
-const listePhrases = ["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le poisson"]
-let choixUser = prompt("mots ou phrases")
-let score = 0
+// Exercice pour manipuler le DOM grace au queryselector
 
-if (choixUser === "mots") {
-    let motUser = prompt("Veuillez taper un mot")
-    for (let i = 0; i < listeMots.length; i++) {
-        if (motUser === listeMots[i]) {
-        score++
-        break
-        }
-        }
-} else {
-    let phraseUser = prompt("Veuillez taper un phrase !")
-    for (let i = 0; i < listePhrases.length; i++) {
-        if (phraseUser === listePhrases[i]) {
-        score++
-        break
-        }
-        }
-}
-
-console.log("Votre score est de " + score)
-
+// Ici je selectionne le contenu de ma div ayant pour id 'zone jeu'
+let divJeu = document.getElementById("zone jeu");
+console.log(divJeu);
 
