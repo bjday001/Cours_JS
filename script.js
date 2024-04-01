cebc98d497a6f0278010cc77710892cdb301337b
bjday001 <jdayinvestis@gmail.com>
2024-04-01 17:46:42 +0200

    test_queryselectorAll

--- script.js
+++ script.js
@@ -2,4 +2,9 @@
 
 // Ici je cible tous les inputs contenu dans toutes les div ayant pur class 'zoneChoix'
 let listeInputRadio = document.querySelectorAll(".zoneChoix input");
-console.log(listeInputRadio);
+//console.log(listeInputRadio);
+
+// Je parcours le contenu de ma variable, qui est un tableau
+for (let i = 0; i < listeInputRadio.length; i++) {
+    console.log(listeInputRadio[i]);
+}
